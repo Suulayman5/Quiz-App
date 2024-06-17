4a3fd1786e8779f9a3eccd905f353b914abe2d84
ajah suleiman <ajahsuleiman@gmail.com>
2024-06-17 16:57:08 +0100

    added questions to array

---
+++ script.js
@@ -0,0 +1,51 @@
+const questions = [
+    {
+        question: 'Which is the largest animal in the world ?',
+        answers:[
+            {text: 'Shark', correct: false},
+            {text: 'Ant', correct: false},
+            {text: 'Blue whale', correct: true},
+            {text: 'Tiger', correct: false},
+        ]
+    },{
+        question: 'Which is the smallest continent in the world ?',
+        answers:[
+            {text: 'Asia', correct: false},
+            {text: 'Australia', correct: true},
+            {text: 'Africa', correct: true},
+            {text: 'Arctic', correct: false},
+        ]
+    },{
+        question: 'How many bones do we have in an ear?',
+        answers:[
+            {text: '5', correct: false},
+            {text: '8', correct: false},
+            {text: '4', correct: false},
+            {text: '3', correct: true},
+        ]
+    },{
+        question: 'How many days are there in July?',
+        answers:[
+            {text: '29', correct: false},
+            {text: '28', correct: false},
+            {text: '31', correct: true},
+            {text: '30', correct: false},
+        ]
+    },{
+        question: 'What is the capital of Finland?',
+        answers:[
+            {text: 'Abuja', correct: false},
+            {text: 'Texes', correct: false},
+            {text: 'Helsinki', correct: true},
+            {text: 'Tokyo', correct: false},
+        ]
+    },{
+        question: 'What language is spoken in Brazil?',
+        answers:[
+            {text: 'English', correct: false},
+            {text: 'Ebira', correct: false},
+            {text: 'Portuguese', correct: true},
+            {text: 'French', correct: false},
+        ]
+    }
+]
